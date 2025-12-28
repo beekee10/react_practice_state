b29fbc83dbf048c33e14770a7e9495edde47e521
Abhishek <aksdoc10@gmail.com>
2025-12-28 12:17:22 +0530

    Added counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Prog1 from './components/Prog1'
 import Prog2 from './components/Prog2'
+import Counter from './components/Counter'
 
 const App = () => {
   return (
