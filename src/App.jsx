9b97015bc67d6d41677c147b696162858aa542c1
Abhishek <aksdoc10@gmail.com>
2025-12-31 12:20:42 +0530

    Prog4 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Prog1 from './components/Prog1'
 import Prog2 from './components/Prog2'
 import Counter from './components/Counter'
+import Prog4 from './components/Prog4'
 
 const App = () => {
   return (
